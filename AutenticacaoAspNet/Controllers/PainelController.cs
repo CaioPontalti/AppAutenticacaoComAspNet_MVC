5d58a23b1b1890e6b3013ba0f84fba45c58946e8
CaioPontalti <caiopontalti@hotmail.com>
2018-12-28 16:34:05 -0200

    Add project files.

---
+++ AutenticacaoAspNet/Controllers/PainelController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutenticacaoAspNet.Controllers
+{
+    public class PainelController : Controller
+    {
+        // GET: Painel
+        [Authorize] //Permite apenas usuários logados acessem a página
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize] //Permite apenas usuários logados acessem a página
+        public ActionResult Mensagens()
+        {
+            return View();
+        }
+    }
+}
